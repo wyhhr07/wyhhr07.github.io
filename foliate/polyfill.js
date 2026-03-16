92505b37b2e856a71928077b276010f632960d63
wan <wyhhr07@gmail.com>
2026-03-16 11:41:42 +0800

    init

--- foliate/polyfill.js
+++ foliate/polyfill.js
@@ -1,15 +1,27 @@
+// Object.groupBy
 if (!Object.groupBy) {
     Object.groupBy = function (items, callback) {
         const result = {};
-
         for (const item of items) {
             const key = callback(item);
-            if (!result[key]) {
-                result[key] = [];
-            }
+            if (!result[key]) result[key] = [];
             result[key].push(item);
         }
-
         return result;
+    };
+}
+
+// Map.groupBy
+if (!Map.groupBy) {
+    Map.groupBy = function (items, callback) {
+        const map = new Map();
+        for (const item of items) {
+            const key = callback(item);
+            if (!map.has(key)) {
+                map.set(key, []);
+            }
+            map.get(key).push(item);
+        }
+        return map;
     };
 }
